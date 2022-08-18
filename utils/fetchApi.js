b7f1d90fd0570d9cdea3d478c2a83c4db7668642
sussan143 <spp26041999@gmail.com>
2022-08-18 23:22:42 +0530

    log .env

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const baseUrl = "https://bayut.p.rapidapi.com";
 
 export const fetchApi = async (url) => {
+  console.log(import.meta.env.VITE_RAPID_API_KEY)
   const { data } = await axios.get(url, {
     headers: {
       "x-rapidapi-host": "bayut.p.rapidapi.com",
